a5c7d601a4bd258b01945d8fa57a4dee1165f459
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2026-08-05 12:56:21 -0300

    test: pin test vscode version and fix fixture paths

--- .vscode-test.js
+++ .vscode-test.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vscode/test-cli');
 
 module.exports = defineConfig({
   files: 'out/test/suite/**/*.test.js',
+  version: '1.124.2',
   mocha: {
     ui: 'tdd',
     timeout: 20000,
